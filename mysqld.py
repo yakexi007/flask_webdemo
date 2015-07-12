5fd38586362a4ef6ff1ff4ef05ead83f5ca57ace
zhangjun <32268834@qq.com>
2015-07-12 14:28:32 +0800

    add new file

---
+++ mysqld.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env	python
+#coding:utf-8
+__author__ = "zhangjun"
+
+import MySQLdb
+
+conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+cur = conn.cursor()
+
+def getData(num=0):
+    sql = "select ip,n_ip,c_room,operator,status from download_nginx_copy limit %s,10;" %num
+    cur.execute(sql)
+    data = cur.fetchall()
+    return data
+
+def serData(ip):
+    sql = "select ip,n_ip,c_room,operator,status from download_nginx_copy where ip LIKE '%%%s%%'" %ip
+    cur.execute(sql)
+    data = cur.fetchall()
+    return data
+
+def delData(ip):
+    sql = "delete from download_nginx_copy where ip = '%s';" %ip
+    cur.execute(sql)
+
+def modData(ip,status):
+    sql = "update download_nginx_copy set status = '%s' where ip = '%s';" %(status,ip)
+    cur.execute(sql)
+
+def addData(ip,n_ip,c_room,operator,status):
+    sql = "insert into download_nginx_copy (ip,n_ip,c_room,operator,status) values('%s','%s','%s','%s','%s')" %(ip,n_ip,c_room,operator,status)
+    cur.execute(sql)
---
+++ static/ajax.js
@@ -0,0 +1,59 @@
+/**
+ * Created by zhangjun on 15/6/19.
+ */
+
+//修改
+function mModel(ip,n_ip,status) {
+    $('#mModal').modal();
+    $('#mip').val(ip);
+    $('#mn_ip').val(n_ip);
+    $('#mstatus').val(status);
+}
+
+function modModel() {
+    var ip = $('#mip').val();
+    var status = $('#mstatus').val();
+    window.location.href = "/mod?ip=" + ip + "&status=" + status;
+}
+
+//添加
+function aModel() {
+    $('#addModal').modal();
+}
+
+function addModel() {
+    var ip = $('#ip').val();
+    var n_ip = $('#n_ip').val();
+    var c_room = $('#c_room').val();
+    var operator = $('#operator').val();
+    var status = $('#status').val();
+    if (ip == '') {
+        alert("IP 不能为空！！！")
+
+    }
+    if (ip != '') {
+        window.location.href = "/add?ip=" + ip + "&n_ip=" + n_ip + "&c_room=" + c_room + "&operator=" + operator + "&status=" + status;
+    }
+}
+
+//删除
+function dModel(ip) {
+    $('#delModal').modal(); //打开模态框
+    $('#info').val(ip);
+}
+
+function delModel() {
+    var ip = $('#info').val(); //根据id获取模态框中的值
+    window.location.href="/delete_ip?ip=" + ip;
+}
+
+//搜索
+function sModel() {
+    var content = $('#s_ip').val();
+    if (content == '') {
+        window.location.href = "/";
+    }
+    if (content != '') {
+        window.location.href = "/search_ip?ip=" + content;
+    }
+}
