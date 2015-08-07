35b8e6ebe79c097247e4ea5c6289cb9b6bf19b83
zhangjun <32268834@qq.com>
2015-08-07 14:48:15 +0800

    Update mysqld.py

--- app1/mysqld.py
+++ app1/mysqld.py
@@ -8,27 +8,52 @@ conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',c
 cur = conn.cursor()
 
 def getData(num=0):
+    try:
+        conn.ping()
+    except:
+        conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+        cur = conn.cursor()
     sql = "select ip,n_ip,c_room,operator,status from download_nginx_copy limit %s,10;" %num
     cur.execute(sql)
     data = cur.fetchall()
     return data
 
 def serData(ip):
+    try:
+        conn.ping()
+    except:
+        conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+        cur = conn.cursor()    
     sql = "select ip,n_ip,c_room,operator,status from download_nginx_copy where ip LIKE '%%%s%%'" %ip
     cur.execute(sql)
     data = cur.fetchall()
     return data
 
 def delData(ip):
+    try:
+        conn.ping()
+    except:
+        conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+        cur = conn.cursor()
     sql = "delete from download_nginx_copy where ip = '%s';" %ip
     cur.execute(sql)
 
 def modData(ip,status):
+    try:
+        conn.ping()
+    except:
+        conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+        cur = conn.cursor()
     sql = "update download_nginx_copy set status = '%s' where ip = '%s';" %('unactive',ip)
     if status == "1":
         sql = "update download_nginx_copy set status = '%s' where ip = '%s';" %('active',ip)
     cur.execute(sql)
 
 def addData(ip,n_ip,c_room,operator,status):
+    try:
+        conn.ping()
+    except:
+        conn = MySQLdb.connect(host='10.16.48.81',user='root',passwd='123',db='weblog',charset='utf8')
+        cur = conn.cursor()    
     sql = "insert into download_nginx_copy (ip,n_ip,c_room,operator,status) values('%s','%s','%s','%s','%s')" %(ip,n_ip,c_room,operator,status)
     cur.execute(sql)
